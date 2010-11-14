578c8eab6ca218f774e00283838e13a34e65c9a3
Valentin Lorentz <progval@gmail.com>
2010-11-14 12:43:55 +0100

    SupyML: update version

--- SupyML/__init__.py
+++ SupyML/__init__.py
@@ -39,7 +39,7 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = "0.1"
+__version__ = "0.2"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 if not hasattr(supybot.authors, 'progval'):
