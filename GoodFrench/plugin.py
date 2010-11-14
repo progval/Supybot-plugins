0de2636975be32d7a2762c2655f016d1dd399727
Valentin Lorentz <progval@gmail.com>
2010-11-14 19:32:20 +0100

    GoodFrench: fix 'il es' detection

--- GoodFrench/plugin.py
+++ GoodFrench/plugin.py
@@ -134,7 +134,7 @@ class SpellChecker:
         self._detect(mode='regexp', correct=firstPerson, mask="j'[^ ]*t")
         self._detect(mode='regexp', correct=firstPerson,mask="je( ne)? [^ ]*t")
         self._detect(mode='regexp', correct=notAS,
-                     mask="(il|elle|on)( ne | n'| )[^ ]+[^u]s\W")
+                     mask="(il|elle|on)( ne | n'| )[^ ]*[^u]s\W")
                      # [^u] is added in order to not detect 'il [vn]ous...'
     def checkSpelling(self):
         self._detect(mode='regexp', correct='quelle', mask='quel [^ ]+ la',
