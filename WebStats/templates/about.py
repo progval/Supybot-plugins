3a431e9b394d5e45fda586c27f1fdec2f14c283a
Valentin Lorentz <progval@gmail.com>
2010-11-20 18:04:22 +0100

    WebStats: create the about page

---
+++ WebStats/templates/about.py
@@ -0,0 +1,30 @@
+import skeleton
+import supybot.utils as utils
+try:
+    from supybot.i18n import PluginInternationalization
+    from supybot.i18n import internationalizeDocstring
+    _ = PluginInternationalization('WebStats')
+except:
+    _ = lambda x:x
+    internationalizeDocstring = lambda x:x
+
+content = _(
+"""<p>This website is powered by three awesome softwares :
+<ul>
+    <li>
+        The well-known <a href="http://python.org">Python</a> programming language
+    </li>
+    <li>
+        <a href="http://supybot.com">Supybot</a>, the greatest Python IRC bot
+    </li>
+    <li>
+        <a href="mailto:progval@gmail.com">ProgVal</a>'s
+        <a href="http://supybot-fr/WebStats">WebStats plugin for Supybot</a>
+    </li>
+""")
+
+def get(useSkeleton):
+    output = content
+    if useSkeleton:
+        output = ''.join([skeleton.start, output, skeleton.end])
+    return output
