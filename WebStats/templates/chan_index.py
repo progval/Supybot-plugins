6822a687db206d10a0ab69f33fe789f2bf3b266a
Valentin Lorentz <progval@gmail.com>
2010-11-21 09:13:25 +0100

    WebStats: Remove debug data

--- WebStats/templates/chan_index.py
+++ WebStats/templates/chan_index.py
@@ -15,7 +15,6 @@ def get(useSkeleton, channel, db):
     channel = '#' + channel
     items = db.getChanGlobalData(channel)
     output = content % (_("Stats about %s channel"), _("There were %n, %n, %n, %n, %n, and %n."))
-    print repr(items)
     output = utils.str.format(output, channel, (items[0], 'line'), (items[1], 'word'),
                                        (items[2], 'char'), (items[3], 'join'),
                                        (items[4], 'part'), (items[5], 'quit'))
