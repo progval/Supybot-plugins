d865e6e164a9b46188c0a706c403e92ac05a0f06
Valentin Lorentz <progval@gmail.com>
2010-11-24 18:34:28 +0100

    WebStats: refactor chan_index.py template + add global nick changes and kicks count

--- WebStats/templates/chan_index.py
+++ WebStats/templates/chan_index.py
@@ -23,28 +23,25 @@ def progressbar(item, max_):
         template %= (item, 0)
     return template
 
+headers = (_('Hour'), _('Lines'), _('Words'), _('Joins'), _('Parts'),
+           _('Quits'), _('Nick changes'), _('Kicks'))
+tableHeaders = '<table><tr>'
+for header in headers:
+    tableHeaders += '<th style="width: 100px;">%s</th>' % header
 def get(useSkeleton, channel, db):
     channel = '#' + channel
     items = db.getChanGlobalData(channel)
     bound = db.getChanRecordingTimeBoundaries(channel)
     output = '<h1>%s</h1>' % _('Stats about %s channel')
     output %= channel
-    output += '<p>%s</p>' % _('There were %n, %n, %n, %n, %n, and %n.')
+    output += '<p>%s</p>' % _('There were %n, %n, %n, %n, %n, %n, %n, '
+                              'and %n.')
     output = utils.str.format(output, (items[0], 'line'), (items[1], 'word'),
                                        (items[2], 'char'), (items[3], 'join'),
-                                       (items[4], 'part'), (items[5], 'quit'))
-    output += """<table><tr>
-                    <th>%s</th>
-                    <th style="width: 100px;">%s</th>
-                    <th style="width: 100px;">%s</th>
-                    <th style="width: 100px;">%s</th>
-                    <th style="width: 100px;">%s</th>
-                    <th style="width: 100px;">%s</th>
-                    <th style="width: 100px;">%s</th>
-                    <th style="width: 100px;">%s</th>
-                </tr>"""
-    output %= (_('Hour'), _('Lines'), _('Words'), _('Joins'), _('Parts'),
-               _('Quits'), _('Nick changes'), _('Kicks'))
+                                       (items[4], 'part'), (items[5], 'quit'),
+                                       (items[6], 'nick change'),
+                                       (items[8], 'kick'))
+    output += tableHeaders
     items = db.getChanXXlyData(channel, 'hour')
     max_ = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     min_hour = 24
@@ -56,25 +53,17 @@ def get(useSkeleton, channel, db):
         for index in range(0, len(max_)):
             max_[index] = max(max_[index], items[hour][index])
     for hour in range(min_hour, max_hour+1):
-        output += """<tr>
-                        <td>%s</td>
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                    </tr>""" % \
-                 (hour,
-                 progressbar(items[hour][0], max_[0]),
-                 progressbar(items[hour][1], max_[1]),
-                 progressbar(items[hour][3], max_[3]),
-                 progressbar(items[hour][4], max_[4]),
-                 progressbar(items[hour][5], max_[5]),
-                 progressbar(items[hour][6], max_[6]),
-                 progressbar(items[hour][8], max_[8])
-                 )
+        output += '<tr><td>%s</td>' % hour
+        for cell in (progressbar(items[hour][0], max_[0]),
+                     progressbar(items[hour][1], max_[1]),
+                     progressbar(items[hour][3], max_[3]),
+                     progressbar(items[hour][4], max_[4]),
+                     progressbar(items[hour][5], max_[5]),
+                     progressbar(items[hour][6], max_[6]),
+                     progressbar(items[hour][8], max_[8])
+                     ):
+            output += cell
+        output += '</tr>'
     output += '</table>'
     if useSkeleton:
         output = ''.join([skeleton.start, output, skeleton.end])
