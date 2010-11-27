5da70afba1f8c3bffed2b76b88918c9cc035cf02
Valentin Lorentz <progval@gmail.com>
2010-11-27 17:11:40 +0100

    WebStats: add forgotten file (templates/nicks.py)

---
+++ WebStats/templates/nicks.py
@@ -0,0 +1,20 @@
+from listingcommons import *
+from listingcommons import _
+
+def get(useSkeleton, channel, db, urlLevel, orderby=None):
+    channel = '#' + channel
+    items = db.getChanGlobalData(channel)
+    bound = db.getChanRecordingTimeBoundaries(channel)
+    output = '<h1>%s</h1>' % _('Stats about %s channel users')
+    output %= channel
+    output = utils.str.format(output, (items[0], 'line'), (items[1], 'word'),
+                                       (items[2], 'char'), (items[3], 'join'),
+                                       (items[4], 'part'), (items[5], 'quit'),
+                                       (items[6], 'nick change'),
+                                       (items[8], 'kick'))
+    items = db.getChanNickGlobalData(channel, 20)
+    output += getTable(_('Nick'), items, channel, urlLevel, orderby)
+
+    if useSkeleton:
+        output = ''.join([skeleton.start, output, skeleton.end])
+    return output
