d5e98dab58762711d45a2b526bb5992d9bcfff72
Valentin Lorentz <progval@gmail.com>
2010-11-27 07:56:56 +0100

    WebStats: fix bug when ordering by nick changes

--- WebStats/templates/chan_index.py
+++ WebStats/templates/chan_index.py
@@ -89,6 +89,9 @@ def get(useSkeleton, channel, db, orderby=None):
     if orderby is None:
         output += fillTable(items, range(min_hour, max_hour+1))
     else:
+        orderby = orderby.split('%20')[0]
+        if not orderby.endswith('s'):
+            orderby += 's'
         index = {'lines':0, 'words':1, 'chars':2, 'joins':3, 'parts':4,
                 'quits':55, 'nicks':6, 'kickers':7, 'kickeds':8}[orderby]
         output += fillTable(items, range(min_hour, max_hour+1), index)
