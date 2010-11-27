eab12ce9161ac2181b52c102ba9cb2b9428c41e5
Valentin Lorentz <progval@gmail.com>
2010-11-27 13:05:21 +0100

    WebStats: change version number 0.2 -> 0.3

--- WebStats/__init__.py
+++ WebStats/__init__.py
@@ -38,7 +38,7 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = "0.2"
+__version__ = "0.3"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 if not hasattr(supybot.authors, 'progval'):
