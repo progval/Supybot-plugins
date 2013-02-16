2d6f1528065066ced4b6b04a6ec77ac57af23719
Valentin Lorentz <progval@gmail.com>
2013-02-16 07:59:51 +0000

    Coffee: Fix Supybot compatibility.

--- Coffee/plugin.py
+++ Coffee/plugin.py
@@ -33,9 +33,16 @@ from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
-from supybot.i18n import PluginInternationalization, internationalizeDocstring
 
-_ = PluginInternationalization('Coffee')
+try:
+    from supybot.i18n import PluginInternationalization
+    from supybot.i18n import internationalizeDocstring
+    _ = PluginInternationalization('Coffee')
+except:
+    # This are useless functions that's allow to run the plugin on a bot
+    # without the i18n plugin
+    _ = lambda x:x
+    internationalizeDocstring = lambda x:x
 
 @internationalizeDocstring
 class Coffee(callbacks.Plugin):
