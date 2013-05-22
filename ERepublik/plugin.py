60348b3d8332d83f411d65ca07d8df6a0787b71d
Valentin Lorentz <progval@progval.net>
2013-05-22 21:05:22 +0200

    ERepublik: Use JSON format and use more customizable architecture.

--- ERepublik/plugin.py
+++ ERepublik/plugin.py
@@ -28,14 +28,14 @@
 
 ###
 
+import re
+import json
+
 import supybot.utils as utils
 from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
-import local.lib as lib
-reload(lib)
-from local.lib import Citizen, Stats, Vojnik, Ekonomija
 try:
     from supybot.i18n import PluginInternationalization
     from supybot.i18n import internationalizeDocstring
@@ -46,31 +46,65 @@ except:
     _ = lambda x:x
     internationalizeDocstring = lambda x:x
 
-def getCitizen(irc, name, cls=Citizen):
-    citizen = cls().loadByName(name)
-    if citizen is None:
-        citizen = Citizen().loadById(name)
-    if citizen is None:
-        irc.error(_('This citizen does not exist.'))
-        return None
-    return citizen
+def getCitizen(irc, name):
+    try:
+        if name.isdigit():
+            base = 'http://api.erpk.org/citizen/profile/%s.json?key=nIKh0F7U'
+            return json.load(utils.web.getUrlFd(base % name))
+        else:
+            base = 'http://api.erpk.org/citizen/search/%s/1.json?key=nIKh0F7U'
+            data = json.load(utils.web.getUrlFd(base % name))
+            return getCitizen(irc, data[0]['id'])
+    except:
+        irc.error(_('This citizen does not exist.'), Raise=True)
+
+def flatten_subdicts(dicts):
+    """Change dict of dicts into a dict of strings/integers. Useful for
+    using in string formatting."""
+    flat = {}
+    for key, value in dicts.items():
+        if isinstance(value, dict):
+            for subkey, subvalue in value.items():
+                if isinstance(subvalue, dict):
+                    for subsubkey, subsubvalue in subvalue.items():
+                        flat['%s__%s__%s' % (key, subkey, subsubkey)] = subsubvalue
+                else:
+                    flat['%s__%s' % (key, subkey)] = subvalue
+        else:
+            flat[key] = value
+    return flat
 
 class ERepublik(callbacks.Plugin):
     threaded = True
 
-    @internationalizeDocstring
-    def info(self, irc, msg, args, name):
-        """<name|id>
+    def _advinfo(self, irc, msg, args, format_, name):
+        """<format> <name|id>
 
-        Displays informations on the given citizen."""
-        citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
-        string = citizen.toString()
-        if string is None:
-            irc.error(_('This citizen does not exist.'))
-        irc.reply(string)
-    info = wrap(info, ['text'])
+        Returns informations about a citizen with advanced formating."""
+        citizen = flatten_subdicts(getCitizen(irc, name))
+        try:
+            irc.replies(map(lambda x:x%citizen, format_.split('\\n')))
+        except KeyError:
+            raise
+            irc.error(_('Invalid format.'), Raise=True)
+    advinfo = wrap(_advinfo, ['something', 'text'])
+
+    def _gen(format_, name, doc):
+        format_ = re.sub('[ \n]+', ' ', format_)
+        def f(self, irc, msg, args, sequence):
+            self._advinfo(irc, msg, args, format_, sequence)
+        f.__doc__ = """<name|id>
+
+        %s""" % doc
+        return wrap(f, ['text'], name=name)
+
+    info = _gen("""Name: %(name)s (ID: %(id)s), Level: %(level)s, Strength:
+    %(strength)s, Residence: %(residence__country__name)s, Citizenship:
+    %(citizenship__name)s, Rank: %(rank__name)s, Party: %(party__name)s, MU:
+    %(army__name)s.
+    """,
+    'info',
+    'Returns general informations about a citizen.')
 
     @internationalizeDocstring
     def medals(self, irc, msg, args, name):
@@ -78,123 +112,11 @@ class ERepublik(callbacks.Plugin):
 
         Displays the citizen's medals."""
         citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
+        medals = ['%s (%i)' % x for x in citizen['medals'].items() if x[1]]
         irc.reply(_('%s has the following medal(s): %s') %
-                  (name, citizen.getMedals()))
+                  (name, ', '.join(medals)))
     medals = wrap(medals, ['text'])
 
-    @internationalizeDocstring
-    def see(self, irc, msg, args, name):
-        """<name|id>
-
-        Displays informations on the given citizen."""
-        citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
-        string = citizen.toString3()
-        if string is None:
-            irc.error(_('This citizen does not exist.'))
-        irc.reply(string)
-    see = wrap(see, ['text'])
-
-    @internationalizeDocstring
-    def money(self, irc, msg, args, name):
-        """<name>
-
-        Displays money's rates."""
-        money = Ekonomija().loadByName(name)
-        money.toString0 = money.toString
-        output = []
-        units = ('gold roon brl frf dem huf cny esp cad usd gbp pln rub '
-                'bgn try grd idriep irr sit pkr hrk').upper().split(' ')
-        for id, unit in enumerate(units):
-            output.append('%s \x02%s\x02' % (getattr(money, 'toString%i' % id)(),
-                                     unit))
-        irc.reply(_(', ').join(output))
-    money = wrap(money, ['text'])
-
-    @internationalizeDocstring
-    def invasion(self, irc, msg, args, channel):
-        """<channel>
-
-        Calls for invasion of the channel."""
-
-        irc.reply(_('PREPARE FOR ORGANIZED JUMP INTO THE CHANNEL'),
-                  prefixNick=False)
-        for i in range(0, 11):
-            irc.reply('%i' % (10-i), prefixNick=False)
-        irc.reply(_('Join %s !') % channel, prefixNick=False)
-    invasion = wrap(invasion, ['validChannel'])
-
-    @internationalizeDocstring
-    def link(self, irc, msg, args, name):
-        """<user|id>
-
-        Returns the link to the user's profile."""
-        citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
-        irc.reply('http://www.erepublik.com/en/citizen/profile/%i' %
-                  citizen.getId())
-    link = wrap(link, ['text'])
-
-    @internationalizeDocstring
-    def donate(self, irc, msg, args, name):
-        """<user|id>
-
-        Returns the link to the user's 'donate' page."""
-        citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
-        irc.reply('http://www.erepublik.com/en/citizen/donate/items/%i' %
-                  citizen.getId())
-    donate = wrap(donate, ['text'])
-
-    @internationalizeDocstring
-    def menu(self, irc, msg, args):
-        """take no arguments
-
-        Returns the eRepublik main links."""
-        links = {'Q5 RIFLES': 'vti-iv-a-weapon-q5/210268',
-                 'Q5 Helici': 'skorpioni-avijacija/237552',
-                 'Q5 Artiljerija': 'skorpionska-artiljerija1/192952'}
-        for link in links:
-            irc.reply('http://economy.erepublik.com/en/company/%s' % link)
-    menu = wrap(menu, [])
-
-    @internationalizeDocstring
-    def land(self, irc, msg, args):
-        """take no arguments
-
-        Returns the eRepublik land links."""
-        links = {'Q1 Iron Rusija': 'vti-iv-iron/193927',
-                 'Q1 Iron Srbija': 'skorpijice-zelezara/222353',
-                 'Q1 Titanijum Rusija': 'skorpioni-titanijum/237601'}
-        for link in links:
-            irc.reply('http://economy.erepublik.com/en/company/%s' % link)
-    land = wrap(land, [])
-
-    @internationalizeDocstring
-    def fight(self, irc, msg, args, name):
-        """<name|id>
-
-        Shows how many damages you can make in one hit."""
-        citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
-        irc.reply(_('damages: %i') % citizen.fightCalcStr(100.0))
-
-    @internationalizeDocstring
-    def kamikaze(self, irc, msg, args, name):
-        """<name|id>
-
-        Shows how many damages you can make in one hit."""
-        citizen = getCitizen(irc, name)
-        if citizen is None:
-            return
-        irc.reply(_('kamikaze attack with full food: %i') %
-                  citizen.fightCalcStr(100.0))
 
 ERepublik = internationalizeDocstring(ERepublik)
 Class = ERepublik
