eb98cea5f368aa3ad00935a2352f8ac996d919ef
Valentin Lorentz <progval@progval.net>
2013-06-01 10:20:57 +0200

    ChannelStatus: Use valid doctype.

--- ChannelStatus/plugin.py
+++ ChannelStatus/plugin.py
@@ -46,7 +46,8 @@ except:
     _ = lambda x:x
 
 PAGE_SKELETON = """\
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Channel status</title>
 <link rel="stylesheet" href="/default.css" />
