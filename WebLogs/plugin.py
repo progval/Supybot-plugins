a20562242cc7ccf6ffdb0a8fc99ce450ac76d4eb
Mikaela Suomalainen <Mkaysi@users.noreply.github.com>
2014-05-30 00:57:50 +0300

    WebLogs: use HTML5.

--- WebLogs/plugin.py
+++ WebLogs/plugin.py
@@ -66,9 +66,10 @@ else:
         return b
 
 page_template = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<!DOCTYPE html>
+<html>
     <head>
+    <meta charset="UTF-8" />
         <title>%(title)s - WebLogs</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <style type="text/css">
